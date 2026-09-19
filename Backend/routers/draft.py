0660bc9c4268717f29a66dadf17d5eb5e40670b5
Aidan <aidan2006@tamu.edu>
2026-09-19 10:31:53 -0500

    Add routes to router files

--- Backend/routers/draft.py
+++ Backend/routers/draft.py
@@ -3,5 +3,6 @@ from sqlalchemy.orm import Session, selectinload
 from sqlalchemy import select
 from database import get_db
 from typing import Annotated
+from schemas import TeamBase, TeamCreate, TeamResponse, DraftRequest, DraftResponse, LeagueResponse, MatchupResponse, PlayerResponse
 
 router = APIRouter()
